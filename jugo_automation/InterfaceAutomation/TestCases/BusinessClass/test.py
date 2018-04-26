e5baf3e5b55df2b483dbf871d1bf1678db0c2d42
chenjinsong <767316856@qq.com>
2018-04-26 22:19:04 +0800

    commit at home

--- jugo_automation/InterfaceAutomation/TestCases/BusinessClass/test.py
+++ jugo_automation/InterfaceAutomation/TestCases/BusinessClass/test.py
@@ -1,11 +1,11 @@
 # -*- coding:utf-8 -*-
-from Method import MD5
-m = MD5.MD5()
-a = m.MD5encodePassword('11111111')
-print a
+# from Method import MD5
+# m = MD5.MD5()
+# a = m.MD5encodePassword('11111111')
+# print a
 from Method import LoadData
 LD = LoadData.LoadData()
 domain = LD.read_domain()
-uil = LD.read_interface_by_name('login')
+uil = LD.read_interface_by_name('sendSmsCode')
 url = LD.montage_url(domain, uil)
 print url
